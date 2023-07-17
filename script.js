6da8ab33d6e908c46638f4f6d7c4a18753d7d183
Zoya Siddiqui <107367922+zoyasiddiqui@users.noreply.github.com>
2023-07-16 20:11:40 -0400

    Create functionality to add elements

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// get the value of the to do list item they typed out
+var emptyList = document.querySelector("ol");
+var counter = 1;
+
+document.getElementById("submit").onclick = function() {
+
+    toDoItem = document.getElementById("item");
+
+    var li = document.createElement('li');
+    var itemID = `item${counter}`;
+    li.setAttribute("id", itemID);
+    
+    var closeButton = document.createElement("button");
+    closeButton.classList.add("close");
+    closeButton.innerText = "x";
+
+    var doneButton = document.createElement("button");
+    doneButton.classList.add("done");
+    doneButton.innerText = "DONE";
+
+    var text = document.createElement("p");
+    text.innerText = toDoItem.value;
+    text.classList.add("to-do-item");
+
+    li.append(text);
+    li.append(closeButton);
+    li.append(doneButton);
+
+    emptyList.append(li);
+    counter += 1;
+    document.getElementById("item").value = "";
+}
+
+
+
